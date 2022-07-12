8b8f28a6b5cefde9a8a110f985854b785233f37c
Cameron Shih <camshih@gmail.com>
2022-07-12 01:12:33 -0400

    hero/main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar';
-
+import Hero from './components/Hero';
 function App() {
   return (
-    <div className="App">
-		<Navbar />
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+    </>
   );
 }
 
