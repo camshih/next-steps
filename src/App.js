e82fcf584ddbdc1137679e50c03161d007950ba8
Cameron Shih <camshih@gmail.com>
2022-07-19 13:04:37 -0400

    classes section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Classes from "./components/Classes";
+
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Classes />
     </>
   );
 }
---
+++ src/components/Classes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Classes = () => {
+  return (
+    <div className="text-white bg-stone-800 text-center uppercase">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae,
+        architecto.
+      </p>
+    </div>
+  );
+};
+
+export default Classes;
